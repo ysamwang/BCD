14be38fc7c5b35ebd956aa8a6c520442d0696149
ysamwang <ysamuelwang@gmail.com>
2015-11-12 11:25:57 -0800

    pre update SE's for EL

--- tests/getFI.R
+++ tests/getFI.R
@@ -1,6 +1,3 @@
-
-
-
 source("tests/simHelper.R")
 
 set.seed(123)
@@ -16,21 +13,24 @@ Sigma <- solve(diag(rep(1, V)) - mod$B.true) %*% mod$Omega.true %*% t(solve(diag
 
 mult.factor <- solve(diag(rep(1,V)) - mod$B.true) %*% t(chol(mod$Omega.true))
 
-
-sim.size <- 2
+omega.true <- mod$Omega.true[lower.tri(mod$Omega.true, diag = T)][which(mod$Omega[lower.tri(mod$Omega, diag = T)]==1)]
+sim.size <- 1000
 n.list <- c(100, 200,500, 1000, 2000)
 
-for(type in c("gauss", "lognormal")){
+for(type in c("gauss", "lognormal")){ 
   record <- data.frame(mse.like = rep(0, length(n.list)),
                        bias.like = rep(0, length(n.list)),
                        mse.el = rep(0, length(n.list)),
                        bias.el = rep(0, length(n.list)))
   
-  ci.record <- matrix(0, nrow = length(n.list), ncol = sum(mod$B) * 2 )
+  ci.record <- matrix(0, nrow = length(n.list), ncol = sum(mod$B) * 2)
+  se.record <- matrix(0, nrow = length(n.list), ncol = sum(mod$B) * 2)
+  entropy.record <- rep(0, length(n.list))
   
   for(n.index in 1:length(n.list)){
     mse.like <- mse.el <- mse.init <- bias.like <- bias.init <- bias.el <- rep(0, sim.size)
-    ci.like <- ci.el <- ci.true <- rep(0, sum(mod$B))
+    ci.like <- ci.el <- se.length.like <- se.length.el <- rep(0, sum(mod$B))
+    entropy <- 0
     for(i in 1:sim.size){
       cat(paste(i,": "))
       n <- n.list[n.index]
@@ -53,7 +53,7 @@ for(type in c("gauss", "lognormal")){
       #### RICF Procedure ####
       out_ricf <- ricf(B = mod$B, Omega = mod$Omega, Y = Y, BInit = NULL,
                                                  OmegaInit = NULL, sigConv = 0, maxIter = 10000,
-                                                 msgs = FALSE, omegaInitScale = .8, tol = 1e-6)
+                                                 msgs = FALSE, omegaInitScale = .9, tol = 1e-6)
       
       
       
@@ -64,7 +64,17 @@ for(type in c("gauss", "lognormal")){
       mse.like[i] <-  mean((beta.like - mod$B.true[which(mod$B==1)])^2)
       bias.like[i] <- mean(beta.like - mod$B.true[which(mod$B==1)])
       
-      se.like <- sqrt(diag(solve(I))/n)[1:sum(mod$B)]
+#       omega.like <- out_ricf$OmegaHat[lower.tri(out_ricf$OmegaHat, diag = T)][which(mod$Omega[lower.tri(mod$Omega, diag = T)]==1)]
+#       mse.like[i] <-  mean((omega.like - omega.true)^2)
+#       bias.like[i] <- mean(omega.like - omega.true)
+
+            
+#       se.like <- sqrt(diag(solve(I))/n)[-c(1:sum(mod$B))]
+#       se.length.like <- se.length.like + se.like 
+#       ci.like <- ci.like +  (abs(omega.like - omega.true) / se.like < qnorm(.975))
+      
+      se.like <- sqrt(diag(solve(I))/n)[c(1:sum(mod$B))]
+      se.length.like <- se.length.like + se.like 
       ci.like <- ci.like +  (abs(beta.like - mod$B.true[which(mod$B==1)]) / se.like < qnorm(.975))
     
       ##### EL Procedure #####
@@ -97,22 +107,21 @@ for(type in c("gauss", "lognormal")){
       bias.el[i] <- mean(optim_out$par - mod$B.true[which(mod$B==1)])
       
       se.el <- sqrt(diag(sw$sw.var)/n)
-      
+      se.length.el <- se.length.el + se.el
       ci.el <- ci.el +  (abs(beta.el - mod$B.true[which(mod$B==1)]) / se.el < qnorm(.975))
-    }
+      entropy <- entropy + -sum(c(1/fitted_mod$d) * log(c(1/fitted_mod$d)))
+    } 
     record$mse.like[n.index] <- mean(mse.like)
     record$bias.like[n.index] <- mean(bias.like)
     record$mse.el[n.index] <- mean(mse.el)
     record$bias.el[n.index] <- mean(bias.el)
     ci.record[n.index, ] <- c(ci.like, ci.el)/ sim.size
+    se.record[n.index, ] <- c(se.length.like, se.length.el)/ sim.size
+    entropy.record[n.index] <- entropy / sim.size
   }
   saveRDS(record, paste("record_", type, sep = ""))
   saveRDS(ci.record, paste("ci_record_", type, sep = ""))
+  saveRDS(se.record, paste("se_record_", type, sep = ""))
+  saveRDS(entropy.record, paste("entropy_record_", type, sep = ""))
 }
 
-
-readRDS("record_gauss")
-readRDS("ci_record_gauss")
-
-readRDS("record_lognormal")
-readRDS("ci_record_lognormal")
