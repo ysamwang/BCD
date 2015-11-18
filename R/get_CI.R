3e2c86eb1d1835ed39697f3857bf2604afb6af49
ysamwang <ysamuelwang@gmail.com>
2015-11-18 11:07:39 -0800

    update doc and code for SE's

--- R/get_CI.R
+++ R/get_CI.R
@@ -1,52 +1,85 @@
-empLike_sandwich <- function(Y, B, Omega, B.hat, p)
+#' Empirical Likelihood Methods for Linear Structural Equation Models
+#' 
+#' Calculate assymptotic variance for non-structural zeros in B and Omega matrix. The ordering of the estimates
+#' in the variance matrix follows standard vec(-) convention indexing over rows first then columns. The elements of B
+#' are ordered before the elements of Omega. Duplicate elements of Omega are not included.
+#'
+#' @param Y V by N matrix with observed data    
+#' @param B V by V matrix with {0,1} giving structure of directed edges
+#' @param Omega V by V matrix with {0,1} giving structure of bi-directed edges
+#' @param B.hat V by V matrix giving estimated edges weights for directed edges
+#' @param Omega.hat V by V matrix giving estimated edge weights for bi-directed edges
+#' @param p vector of length N containing the estimated empirical likelihood weights
+#' @return The asymptotic covariance matrix as derived in Qin and Lawless 1994
+#' @export
+getELVariance <- function(Y, B, Omega, B.hat, Omega.hat, p)
 {
+  # Index of non-zero B's in proper order (column by column)
   beta.index <- getBetaIndex(B)
+  # Index of non-zero Omega's
   omega.index <- getOmegaIndex(Omega)
+  # number of Nodes
   V <- dim(B)[1]
+  # errors for each node and observation
   errors <- ((diag(rep(1,V)) - B.hat) %*% Y)  #- mu.hat
-  g.i <- rep(0, V + dim(omega.index)[1])
-
   
-  # holds overall diff
-  dg <- matrix(0, nrow = V + dim(omega.index)[1],
-               ncol =  dim(beta.index)[1])
-  g.var <- matrix(0, nrow = V + dim(omega.index)[1],
-                  ncol = V + dim(omega.index)[1])
+  
+  # pre-allocate memory (note that dg.i is cleared out in each iteration of the loop)
+  dg <- matrix(0, nrow = V + V * (V + 1) /2,
+               ncol =  dim(beta.index)[1] + dim(omega.index)[1])
+  g.var <- matrix(0, nrow = V + V * (V + 1) /2,
+                  ncol = V + V * (V + 1) /2)
+  g.i <- rep(0, V + V * (V+1)/2)
+  
+  
+  
+  # indexes of non-replicated entries of Omega
+  # This includes 0 entries as well whereas Omega.index only includes non-zero entries
+  covar.temp <- matrix(0, ncol = 2, nrow = V * (V + 1) /2)
+  count <- 1
+  for(i in 1:V){
+    # TO-DO: modify code so it is similar to Omega.index in cbinding to null
+    # probably more readable (could be tad slower)
+    covar.temp[c(count:(count + V -i)), c(1:2)] <- cbind(c(i:V), rep(i, (V-i+1)))
+    count <- count + (V-i+1)
+  }
+  
   
   for(n in 1:length(p)){
+    ### Form m(X_i, \theta ) ### 
+    
+    # mean restrictions
     g.i[1:V] <- errors[, n]
-    g.i[-c(1:V)] <- errors[omega.index[, 1],n] * errors[omega.index[, 2],n]
+    # covariance restrictions
+    g.i[-c(1:V)] <- errors[covar.temp[, 1], n] * errors[covar.temp[, 2] ,n] - Omega.hat[covar.temp]
     
     g.var <- g.var + p[n] * g.i %*% t(g.i)
     
     
-    
-    dg.i <- matrix(0, nrow = V + dim(omega.index)[1],
-                   ncol = dim(beta.index)[1])
+    ### Now form dm/dtheta
+    dg.i <- matrix(0, nrow = V + V * (V + 1)/2,
+                   ncol = dim(beta.index)[1] + dim(omega.index)[1])
     
     # derivative of mean constraints
-    for(i in 1:V){
-      dg.i[i, which(beta.index[,1] == i)] <- -Y[beta.index[which(beta.index[,1] == i), 2], n]
+    for(k in 1:V){
+      dg.i[k, which(beta.index[, 1] == k)] <- -Y[beta.index[which(beta.index[, 1] == k), 2], n]
     }
     
-    for(j in 1:dim(omega.index)[1]){
-      
-      # derivative of beta's
-      dg.i[j+V, which(beta.index[,1] == omega.index[j,1])] <- -Y[beta.index[which(beta.index[,1] == omega.index[j,1]), 2], n] *
-        errors[omega.index[j,2], n]
-      dg.i[j+V, which(beta.index[,1] == omega.index[j,2])] <- -Y[beta.index[which(beta.index[,1] == omega.index[j,2]), 2], n] *
-        errors[omega.index[j,1], n]
+    for(j in 1:(V * (V+1) / 2)){
+      dg.i[V + j, which(beta.index[,1] == covar.temp[j,1])] <- -Y[beta.index[which(beta.index[,1] == covar.temp[j, 1]), 2], n] * errors[covar.temp[j,2], n]
+      dg.i[V + j, which(beta.index[,1] == covar.temp[j,2])] <- -Y[beta.index[which(beta.index[,1] == covar.temp[j, 2]), 2], n] * errors[covar.temp[j,1], n]
+      if(Omega[covar.temp[j,1],covar.temp[j,2]] == 1){
+        dg.i[V + j, dim(beta.index)[1] + which(covar.temp[j,1] == omega.index[,1] & covar.temp[j,2] == omega.index[,2])] <- -1 
+      }
     }
     dg <- dg + dg.i * p[n]
   }
   
-  return(list(dg = dg,
-              g.var = g.var,
-              sw.var = solve(t(dg) %*% solve(g.var) %*% dg)))
+  return(solve(t(dg) %*% solve(g.var) %*% dg))
 }
 
 
-#################
+## Helper function for emp
 getBetaIndex <- function(B)
 {
   ret <- NULL
@@ -59,13 +92,29 @@ getBetaIndex <- function(B)
 getOmegaIndex <- function(Omega){ 
   ret <- NULL
   V <- dim(Omega)[1]
-  for(i in 1:(V-1)){
-    ret <- rbind(ret, cbind(which(Omega[(i+1):V, i]==0)+i, rep(i, sum(Omega[(i+1):V, i]==0))))
+  for(i in 1:V){
+    ret <- rbind(ret, cbind(which(Omega[i:V, i]==1)+i-1, rep(i, sum(Omega[i:V, i]==1))))
   } 
   return(ret)
 }
 
-ricf_info <- function(S, BHat, OmegaHat, B, Omega)
+
+
+#' Maximum Likelihood Method for Linear Structural Equation Models
+#' 
+#' Calculate Fisher information for non-structural zeros in B and Omega matrix. The ordering of the estimates
+#' in the Information matrix follows standard vec(-) convention indexing over rows first then columns. The elements of B
+#' are ordered before the elements of Omega. Duplicate elements of Omega are not included.
+#'
+#' @param S V by V matrix corresponding to the sample covariance    
+#' @param B V by V matrix with {0,1} giving structure of directed edges
+#' @param Omega V by V matrix with {0,1} giving structure of bi-directed edges
+#' @param B.hat V by V matrix giving estimated edges weights for directed edges
+#' @param Omega.hat V by V matrix giving estimated edge weights for bi-directed edges
+#' @param type string describing which Fisher Information to calculate. Options are "expected" or "observed".
+#' @return The Fisher information matrix as derived by Fox and Drton 2014
+#' @export
+getRicfInfo <- function(S, B, Omega,  B.hat, Omega.hat, type = "expected")
 {
   V <- dim(B)[1]
   ### Create P Matrix ####
@@ -73,6 +122,8 @@ ricf_info <- function(S, BHat, OmegaHat, B, Omega)
   temp <- matrix(c(which(c(B)==1),1:sum(B)), nrow = sum(B), ncol = 2)
   P[temp] <- 1
   Q <- matrix(0, nrow = V^2, ncol = (sum(Omega)-V)/2 + V)
+  
+  ## TO-DO: Possibly use subsetting with direct insertion
   Omega.temp <- matrix(0, nrow = V, ncol = V)
   count <- 0
   for(i in 1:V){
@@ -95,11 +146,25 @@ ricf_info <- function(S, BHat, OmegaHat, B, Omega)
   I <- matrix(0, nrow = sum(B) + (sum(Omega)-V)/2 + V,
               ncol = sum(B) + (sum(Omega)-V)/2 + V)
   
-  I[1:sum(B), 1:sum(B)] <- t(P) %*% (S %x% omega.inv + (eye.minus.b.inv %x% t(eye.minus.b.inv)) %*% Tr) %*% P
-  I[-c(1:sum(B)),-c(1:sum(B))] <- 1/2 * t(Q) %*% (omega.inv %x% omega.inv) %*% Q
-  I[c(1:sum(B)),-c(1:sum(B))] <- t(P) %*% ( eye.minus.b.inv %x% omega.inv) %*% Q
-  I[-c(1:sum(B)),c(1:sum(B))] <- t(Q) %*% (t(eye.minus.b.inv) %x% omega.inv) %*% P
-  
+  if(type == "expected"){
+    I[1:sum(B), 1:sum(B)] <- t(P) %*% (S %x% omega.inv + (eye.minus.b.inv %x% t(eye.minus.b.inv)) %*% Tr) %*% P
+    I[-c(1:sum(B)),-c(1:sum(B))] <- 1/2 * t(Q) %*% (omega.inv %x% omega.inv) %*% Q
+    I[c(1:sum(B)),-c(1:sum(B))] <- t(P) %*% ( eye.minus.b.inv %x% omega.inv) %*% Q
+    I[-c(1:sum(B)),c(1:sum(B))] <- t(Q) %*% (t(eye.minus.b.inv) %x% omega.inv) %*% P
+  } else if (type == "observed"){
+    back_term <- omega.inv %*% (diag(rep(1, V)) - BHat) %*% S %*% t((diag(rep(1, V)) - BHat)) %*% omega.inv
+    I[1:sum(B), 1:sum(B)] <- t(P) %*% (S %x% omega.inv + (eye.minus.b.inv %x% t(eye.minus.b.inv)) %*% Tr) %*% P
+    # notice difference between this and expected
+    I[-c(1:sum(B)),-c(1:sum(B))] <- 1/2 * t(Q) %*% 
+      ((omega.inv %x% omega.inv) -
+         (omega.inv %x% back_term) - 
+         (back_term %x% omega.inv)) %*% Q
+    
+    I[c(1:sum(B)),-c(1:sum(B))] <- t(P) %*% ((S %*% t(diag(rep(1, V)) - BHat) %*% eye.minus.b.inv) %x% omega.inv) %*% Q
+    I[-c(1:sum(B)),c(1:sum(B))] <- t(I[c(1:sum(B)),-c(1:sum(B))])
+  } else {
+    stop("Argument type not valid")
+  }
   
   return(I)
 }
--- tests/getFI.R
+++ tests/getFI.R
@@ -1,8 +1,9 @@
-source("tests/simHelper.R")
-
+library('BCD')
+library('MASS')
+# setwd("~/BCD")
 set.seed(123)
 V <- 8
-n <- 2
+n <- 1000
 k <- 3
 b <- .3
 d <- .3
@@ -14,26 +15,32 @@ Sigma <- solve(diag(rep(1, V)) - mod$B.true) %*% mod$Omega.true %*% t(solve(diag
 mult.factor <- solve(diag(rep(1,V)) - mod$B.true) %*% t(chol(mod$Omega.true))
 
 omega.true <- mod$Omega.true[lower.tri(mod$Omega.true, diag = T)][which(mod$Omega[lower.tri(mod$Omega, diag = T)]==1)]
-sim.size <- 1000
-n.list <- c(100, 200,500, 1000, 2000)
+b.true <- mod$B.true[which(mod$B==1)]
+sim.size <- 500
+n.list <- c(100, 200,500, 1000, 5000)
 
 for(type in c("gauss", "lognormal")){ 
-  record <- data.frame(mse.like = rep(0, length(n.list)),
-                       bias.like = rep(0, length(n.list)),
-                       mse.el = rep(0, length(n.list)),
-                       bias.el = rep(0, length(n.list)))
+  record <- data.frame(b.mse.like = rep(0, length(n.list)),
+                       b.bias.like = rep(0, length(n.list)),
+                       b.mse.el = rep(0, length(n.list)),
+                       b.bias.el = rep(0, length(n.list)),
+                       o.mse.like = rep(0, length(n.list)),
+                       o.bias.like = rep(0, length(n.list)),
+                       o.mse.el = rep(0, length(n.list)),
+                       o.bias.el = rep(0, length(n.list)))
   
-  ci.record <- matrix(0, nrow = length(n.list), ncol = sum(mod$B) * 2)
-  se.record <- matrix(0, nrow = length(n.list), ncol = sum(mod$B) * 2)
+  ci.record <- matrix(0, nrow = length(n.list), ncol = (sum(mod$B) + (sum(mod$Omega) - V)/2 + V) * 2)
+  se.record <- matrix(0, nrow = length(n.list), ncol = (sum(mod$B) + (sum(mod$Omega) - V)/2 + V) * 2)
   entropy.record <- rep(0, length(n.list))
-  
   for(n.index in 1:length(n.list)){
-    mse.like <- mse.el <- mse.init <- bias.like <- bias.init <- bias.el <- rep(0, sim.size)
-    ci.like <- ci.el <- se.length.like <- se.length.el <- rep(0, sum(mod$B))
+    b.mse.like <- b.mse.el <- o.mse.like <- o.mse.el <- b.bias.like <- b.bias.el <- o.bias.like <- o.bias.el <- rep(0, sim.size)
+    ci.like <- ci.el <- se.length.like <- se.length.el <- rep(0, sum(mod$B) + (sum(mod$Omega) - V)/2 + V)
     entropy <- 0
+    n <- n.list[n.index]
+    cat(paste("\n===",n, type,"\n"))
     for(i in 1:sim.size){
       cat(paste(i,": "))
-      n <- n.list[n.index]
+
       
       if(type == "gauss"){
         Y <- t(MASS::mvrnorm(n = n, Sigma = Sigma, mu = rep(0, V)))
@@ -61,34 +68,28 @@ for(type in c("gauss", "lognormal")){
                      B = mod$B, Omega = mod$Omega)
       
       beta.like <- out_ricf$BHat[which(mod$B==1)]
-      mse.like[i] <-  mean((beta.like - mod$B.true[which(mod$B==1)])^2)
-      bias.like[i] <- mean(beta.like - mod$B.true[which(mod$B==1)])
+      omega.like <- out_ricf$OmegaHat[lower.tri(mod$Omega.true, diag = T) & mod$Omega ==1]
       
-#       omega.like <- out_ricf$OmegaHat[lower.tri(out_ricf$OmegaHat, diag = T)][which(mod$Omega[lower.tri(mod$Omega, diag = T)]==1)]
-#       mse.like[i] <-  mean((omega.like - omega.true)^2)
-#       bias.like[i] <- mean(omega.like - omega.true)
-
-            
-#       se.like <- sqrt(diag(solve(I))/n)[-c(1:sum(mod$B))]
-#       se.length.like <- se.length.like + se.like 
-#       ci.like <- ci.like +  (abs(omega.like - omega.true) / se.like < qnorm(.975))
+      b.mse.like[i] <-  mean((beta.like - b.true)^2)
+      b.bias.like[i] <- mean(beta.like - b.true)
       
-      se.like <- sqrt(diag(solve(I))/n)[c(1:sum(mod$B))]
+      o.mse.like[i] <-  mean((omega.like - omega.true)^2)
+      o.bias.like[i] <- mean(omega.like - omega.true)
+      
+      se.like <- sqrt(diag(solve(I))/n)
       se.length.like <- se.length.like + se.like 
-      ci.like <- ci.like +  (abs(beta.like - mod$B.true[which(mod$B==1)]) / se.like < qnorm(.975))
+      ci.like <- ci.like +  ((abs(c(beta.like - b.true, omega.like - omega.true))  / se.like) < 1.959964)
     
       ##### EL Procedure #####
       
       out_ricf_init <- ricf(B = mod$B, Omega = mod$Omega, Y = Y, BInit = NULL,
                             OmegaInit = NULL, sigConv = 0, maxIter = 1,
-                            msgs = FALSE, omegaInitScale = .9)
+                            msgs = FALSE, omegaInitScale = .8)
       
       
       num_dual_vars = V + sum(mod$Omega == 0)/2
       init_val = c(out_ricf_init$BHat[mod$B==1])
-      mse.init[i] <- mean((init_val - mod$B.true[which(mod$B==1)])^2)
-      bias.init[i] <- mean(init_val - mod$B.true[which(mod$B==1)])
-      
+
       optim_out <- optim(par = init_val, fn = sem_el_fit_obj, gr = NULL,
                          y_r = Y, omega_r = mod$Omega, b_r = mod$B,
                          dual_r = rep(0, num_dual_vars), tol = 1e-6, max_iter = 60,
@@ -96,32 +97,58 @@ for(type in c("gauss", "lognormal")){
                          method = "BFGS", control = list(fnscale = -1))
       
       fitted_mod <- sem_el_fit_weights(optim_out$par,y_r = Y, omega_r = mod$Omega, b_r = mod$B,
-                                       dual_r = rep(0, num_dual_vars), tol = 1e-6, max_iter = 100, meanEst = 0)
+                                       dual_r = rep(0, num_dual_vars), tol = 1e-6, max_iter = 60, meanEst = 0)
+      
       B.hat <- matrix(0, nrow = V, ncol = V)
       B.hat[mod$B==1] <- optim_out$par
+      Omega.hat <- (diag(rep(1,V)) - B.hat) %*% Y %*% diag(c(1/fitted_mod$d)) %*% t(Y) %*% t((diag(rep(1,V)) - B.hat))
+      omega.el <- Omega.hat[lower.tri(Omega.hat, diag = TRUE) & mod$Omega ==1]
       
-      sw <- empLike_sandwich(Y, mod$B, mod$Omega, B.hat, c(1/fitted_mod$d))
+      sw <- empLike_sandwich(Y, mod$B, mod$Omega, B.hat, Omega.hat, c(1/fitted_mod$d))
       
-      beta.el <- B.hat[which(mod$B==1)]
-      mse.el[i] <- mean((optim_out$par - mod$B.true[which(mod$B==1)])^2)
-      bias.el[i] <- mean(optim_out$par - mod$B.true[which(mod$B==1)])
+      beta.el <- optim_out$par
+      b.mse.el[i] <- mean((beta.el - b.true)^2)
+      b.bias.el[i] <- mean(beta.el - b.true)
+      
+      o.mse.el[i] <- mean((omega.el - omega.true)^2)
+      o.bias.el[i] <- mean(omega.el - omega.true)
       
       se.el <- sqrt(diag(sw$sw.var)/n)
       se.length.el <- se.length.el + se.el
-      ci.el <- ci.el +  (abs(beta.el - mod$B.true[which(mod$B==1)]) / se.el < qnorm(.975))
+      ci.el <- ci.el +  ((abs(c(beta.el - b.true,  omega.el - omega.true))  / se.el) < 1.959964)
       entropy <- entropy + -sum(c(1/fitted_mod$d) * log(c(1/fitted_mod$d)))
     } 
-    record$mse.like[n.index] <- mean(mse.like)
-    record$bias.like[n.index] <- mean(bias.like)
-    record$mse.el[n.index] <- mean(mse.el)
-    record$bias.el[n.index] <- mean(bias.el)
+    record$b.mse.like[n.index] <- mean(b.mse.like)
+    record$b.bias.like[n.index] <- mean(b.bias.like)
+    record$b.mse.el[n.index] <- mean(b.mse.el)
+    record$b.bias.el[n.index] <- mean(b.bias.el)
+    
+    record$o.mse.like[n.index] <- mean(o.mse.like)
+    record$o.bias.like[n.index] <- mean(o.bias.like)
+    record$o.mse.el[n.index] <- mean(o.mse.el)
+    record$o.bias.el[n.index] <- mean(o.bias.el)
+    
+    
     ci.record[n.index, ] <- c(ci.like, ci.el)/ sim.size
     se.record[n.index, ] <- c(se.length.like, se.length.el)/ sim.size
     entropy.record[n.index] <- entropy / sim.size
   }
-  saveRDS(record, paste("record_", type, sep = ""))
-  saveRDS(ci.record, paste("ci_record_", type, sep = ""))
-  saveRDS(se.record, paste("se_record_", type, sep = ""))
-  saveRDS(entropy.record, paste("entropy_record_", type, sep = ""))
+  saveRDS(record, paste("record_", type, ".RDS", sep = ""))
+  saveRDS(ci.record, paste("ci_record_", type, ".RDS", sep = ""))
+  saveRDS(se.record, paste("se_record_", type, ".RDS", sep = ""))
+  saveRDS(entropy.record, paste("entropy_record_", type,".RDS", sep = ""))
 }
 
+# type = "lognormal"
+# record <- readRDS(paste("record_", type, ".RDS", sep = ""))
+# ci.record <- readRDS(paste("ci_record_", type, ".RDS", sep = ""))
+# se.record <- readRDS(paste("se_record_", type, ".RDS", sep = ""))
+# entropy.record <- readRDS(paste("entropy_record_", ".RDS", type, sep = ""))
+# 
+# 
+# plot(ci.record[,24],
+#      xlim = c(1,5), type= "b", ylim = c(.3,1))
+# for(i in 25:46){
+#   lines(ci.record[,i],type = "b")
+# }
+# abline(h = .95, lwd = 2, col = "red")
